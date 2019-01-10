045ea7c3c35d45c175436fbebe8b6a9b28d1b1a0
bejalane <dudukero@gmail.com>
2019-01-10 04:35:52 +0200

    agency page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 jQuery(document).ready( function ($) {
 
+    function agencyPeopleLastTitleColor(){
+        var people = $(".agency-people-col").length;
+        var peoplePerRow = 3;
+        //IF WE ADD MEDIA QUERIES AND PEOPLE PER ROW WILL CHANGE USE THIS CASES:
+        var mq1 = window.matchMedia( "(max-width: 768px)" );
+        var mq2 = window.matchMedia( "(max-width: 420px)" );
+        if (mq1.matches) {
+            peoplePerRow = 2;
+        }
+        if (mq2.matches) {
+            peoplePerRow = 1;
+        }
+        var lastRowCount = (people%peoplePerRow) ? people%peoplePerRow : peoplePerRow;
+        for(var i = people - 1; i > (people - lastRowCount -1); i--){
+            var block = $(".agency-people-col")[i];
+            $(block).children(".name").css({"color": "#fafafa"});
+        }
+    }
+    agencyPeopleLastTitleColor();
+
+
+/*
     // about lighgallery
 
     $('#about-gallery').lightGallery({
@@ -295,19 +317,7 @@ jQuery(document).ready( function ($) {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 });
